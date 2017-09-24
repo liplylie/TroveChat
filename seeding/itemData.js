716122d6eb7827380447b4cbf23146b947f5d5a7
Dhruv <dhruvp95@gmail.com>
2017-09-23 17:54:49 -0700

    update item seed data

--- seeding/itemData.js
+++ seeding/itemData.js
@@ -1,7 +1,7 @@
 module.exports = [
   //items belonging to user 1
   {
-    rentee_id: 1,
+    rentee_id: 2,
     itemname: 'WOOL BOMBER JACKET WITH APPLIQUES',
     image: 'https://media.gucci.com/style/DarkGray_Center_0_0_1200x1200/1491471014/474727_X5T33_6564_001_100_0000_Light-Wool-bomber-jacket-with-appliqus.jpg',
     brand: 'Gucci',
@@ -12,7 +12,7 @@ module.exports = [
     status: 'Available'
   },
   {
-    rentee_id: 1,
+    rentee_id: 2,
     itemname: 'TEDDY LIGHTWEIGHT "LOVE" JACKET IN MULTICOLOR VISCOSE',
     image: 'https://cdn.yoox.biz/34/34711326BR_13_f.jpg',
     brand: 'Saint Laurent',
@@ -23,7 +23,7 @@ module.exports = [
     status: 'Available'
   },
   {
-    rentee_id: 1,
+    rentee_id: 2,
     itemname: 'LANDSCAPE JACQUARD JERSEY T-SHIRT',
     image: 'https://assets.prada.com/content/dam/prada-ecommerce/images/details/UJN443_1NOG_F0124_S_172-1.jpg/_jcr_content/renditions/original',
     brand: 'Prada',
@@ -34,7 +34,7 @@ module.exports = [
     status: 'Available'
   },
   {
-    rentee_id: 1,
+    rentee_id: 2,
     itemname: 'BLACK T-SHIRT WITH MULTICOLOR PATCHES',
     image: 'https://images.www.fendi.com/images/hbc/h94/8835962437662/FY0682A5CFF0QA1_01_MediumSize#product-medium',
     brand: 'Fendi Roma',
@@ -45,7 +45,7 @@ module.exports = [
     status: 'Available'
   },
   {
-    rentee_id: 1,
+    rentee_id: 2,
     itemname: 'ARMY PARKA WITH SHEARLING LINING',
     image: 'http://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-army-parka-with-shearling-lining-ready-to-wear--HDK05WANU652_PM2_Front%20view.jpg?wid=1214&hei=1214',
     brand: 'Louis Vuitton',
@@ -54,6 +54,185 @@ module.exports = [
     sex: 'M',
     tag: 'coat parka cotton hoodie cold',
     status: 'Available'
-  }
+  },
+  {
+    rentee_id: 2,
+    itemname: 'GUCCI COCO CAPITAN LOGO T-SHIRT',
+    image: 'https://media.gucci.com/style/DarkGray_Center_0_0_1200x1200/1498151717/493117_X3H99_9823_001_100_0000_Light-Gucci-Coco-Capitn-logo-T-shirt.jpg',
+    brand: 'Gucci',
+    price: 550.00,
+    size: 'Large',
+    sex: 'M',
+    tag: 't-shirt white gucci relaxed',
+    status: 'Available'
+  },
+  {
+    rentee_id: 2,
+    itemname: 'THE ICONIC G-1 BOMBER JACKET',
+    image: 'http://www.ralphlauren.com/graphics/product_images/pPOLO2-26489740_lifestyle_dt.jpg',
+    brand: 'Ralph Lauren',
+    price: 1195.00,
+    size: 'Medium',
+    sex: 'M',
+    tag: 'jacket bomber brown',
+    status: 'Available'
+  },
+
+  {
+    rentee_id: 1,
+    itemname: 'EMBROIDERED CLUNY LACE DRESS',
+    image: 'https://media.gucci.com/style/DarkGray_Center_0_0_1200x1200/1494617411/475282_ZJD16_4920_001_100_0000_Light-Embroidered-cluny-lace-dress.jpg',
+    brand: 'Gucci',
+    price: 3980.00,
+    size: '38',
+    sex: 'F',
+    tag: 'lace dress jeweled ruffle',
+    status: 'Available'
+  },
+  {
+    rentee_id: 1,
+    itemname: 'JERSEY SHIRTDRESS WITH LUREX TRIM',
+    image: 'https://media.gucci.com/style/DarkGray_Center_0_0_1200x1200/1481113806/467505_X5R17_1048_001_100_0000_Light-Jersey-shirtdress-with-lurex-trim.jpg',
+    brand: 'Gucci',
+    price: 3900.00,
+    size: 'Small',
+    sex: 'F',
+    tag: 'dress unlined a-line back-zip',
+    status: 'Available'
+  },
+  {
+    rentee_id: 1,
+    itemname: 'BUTTERFLY PRINT SHIRT WITH RUFFLE DETAIL',
+    image: 'https://media.gucci.com/style/DarkGray_Center_0_0_1200x1200/1500916510/493596_ZJQ84_1868_001_100_0000_Light-Butterfly-print-shirt-with-ruffle-detail.jpg',
+    brand: 'Gucci',
+    price: 1800.00,
+    size: '36',
+    sex: 'F',
+    tag: 'butterfly dress floral ruffle print',
+    status: 'Available'
+  },
+  {
+    rentee_id: 1,
+    itemname: 'BLACK LACE ONE SHOULDER ASYMMETRICAL MINI DRESS',
+    image: 'https://cdn.yoox.biz/34/34711296WG_13_f.jpg',
+    brand: 'Saint Laurent',
+    price: 3190.00,
+    size: '36',
+    sex: 'F',
+    tag: 'dress one-shoulder black lace',
+    status: 'Available'
+  },
+  {
+    rentee_id: 1,
+    itemname: 'CHEVRON OVER COAT',
+    image: 'https://images.www.fendi.com/images/hcf/h18/8839427588126/FF8426A0MVF03N9_01_MediumSize#product-medium',
+    brand: 'Fendi Roma',
+    price: 4990.00,
+    size: '38',
+    sex: 'F',
+    tag: 'wool silk overcoat',
+    status: 'Available'
+  },
+  {
+    rentee_id: 1,
+    itemname: 'GRAPHIC CHAIN PRINTED SHIRT',
+    image: 'http://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-chain-printed-shirt-ready-to-wear--FDBL02DJD620_PM2_Front%20view.jpg?wid=1214&hei=1214',
+    brand: 'Louis Vuitton',
+    price: 1410.00,
+    size: '34',
+    sex: 'F',
+    tag: 'graphic shirt print chain',
+    status: 'Available'
+  },
+  {
+    rentee_id: 1,
+    itemname: 'WOOL JACQUARD PULLOVER',
+    image: 'http://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-wool-jacquard-pullover-ready-to-wear--FDKL55FBZ001_PM2_Front%20view.jpg?wid=1214&hei=1214',
+    brand: 'Louis Vuitton',
+    price: 1160.00,
+    size: 'Small',
+    sex: 'F',
+    tag: 'long-sleeve black print ',
+    status: 'Available'
+  },
+
+  {
+    rentee_id: 3,
+    itemname: 'REGULAR PRINTED SHIRT',
+    image: 'http://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-regular-printed-shirt-ready-to-wear--HDS08WDDY001_PM2_Front%20view.jpg?wid=1214&hei=1214',
+    brand: 'Louis Vuitton',
+    price: 835.00,
+    size: 'Medium',
+    sex: 'M',
+    tag: 'shirt print',
+    status: 'Available'
+  },
+  {
+    rentee_id: 3,
+    itemname: 'EMBROIDERED VARSITY JACKET',
+    image: 'http://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-embroidered-varsity-jacket-ready-to-wear--HDB52WAIS920_PM2_Front%20view.jpg?wid=1214&hei=1214',
+    brand: 'Louis Vuitton',
+    price: 2700.00,
+    size: 'Medium',
+    sex: 'M',
+    tag: 'jacket lightweight varsity',
+    status: 'Available'
+  },
+  {
+    rentee_id: 3,
+    itemname: 'STUDDED STANDARD WALE COURDUROY BLOUSON JACKET',
+    image: 'https://assets.prada.com/content/dam/prada-ecommerce/images/details/SGN569_1OP2_F0346_S_172-1.jpg/_jcr_content/renditions/original',
+    brand: 'Prada',
+    price: 4810.00,
+    size: 'Small',
+    sex: 'M',
+    tag: 'tan bomber jacket',
+    status: 'Available'
+  },
+  {
+    rentee_id: 3,
+    itemname: 'BADGE JERSEY T-SHIRT',
+    image: 'https://assets.prada.com/content/dam/prada-ecommerce/images/details/UJN400_1PY1_F0009_S_171-1.jpg/_jcr_content/renditions/original',
+    brand: 'Prada',
+    price: 640.00,
+    size: 'Medium',
+    sex: 'M',
+    tag: 't-shirt white short-sleeves',
+    status: 'Available'
+  },
+  {
+    rentee_id: 3,
+    itemname: 'STRETCH COTTON PATCH POLO SHIRT',
+    image: 'https://assets.prada.com/content/dam/prada-ecommerce/images/details/UJN447_REV_F0VWE_S_172-1.jpg/_jcr_content/renditions/original',
+    brand: 'Prada',
+    price: 550.00,
+    size: 'Medium',
+    sex: 'M',
+    tag: 'polo shirt',
+    status: 'Available'
+  },
+  {
+    rentee_id: 3,
+    itemname: 'WOOL KNITTED SWEATER WITH CHIROPTERA APPLIQUE',
+    image: 'https://media.gucci.com/style/DarkGray_Center_0_0_1200x1200/1498151705/487268_X9H08_9511_001_100_0000_Light-Wool-knitted-sweater-with-Chiroptera-appliqu.jpg',
+    brand: 'Gucci',
+    price: 1690.00,
+    size: 'Large',
+    sex: 'M',
+    tag: 'sweater wool knit',
+    status: 'Available'
+  },
+  {
+    rentee_id: 3,
+    itemname: 'TECHNICAL JERSEY SWEATSHIRT',
+    image: 'https://media.gucci.com/style/DarkGray_Center_0_0_1200x1200/1499338809/475354_X5U11_1014_001_100_0000_Light-Technical-jersey-sweatshirt.jpg',
+    brand: 'Gucci',
+    price: 120.00,
+    size: 'Medium',
+    sex: 'M',
+    tag: 'black hoodie',
+    status: 'Available'
+  },
+  
 
 ]
