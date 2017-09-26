7584788649ebeb6d8bc51b717a61decfcb421519
Alex Ko <alexkosj@gmail.com>
2017-09-26 14:20:12 -0700

    implement filtering by sex and women comps

--- client/src/components/Men.js
+++ client/src/components/Men.js
@@ -1,31 +1,17 @@
 import React, { Component } from 'react';
-import MenItem from './MenItem'
+import MenItem from './MenItem';
 
 class Men extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      maleItems: []
-    }
   }
-
-  // componentDidMount() {
-  //   this.filterItems();
-  // }
-
-  // filterItems() {
-  //   this.state.maleItems = this.props.passItems.filter(item => {
-  //     return item.sex === 'M';
-  //   });
-  // }
 
   render() {
     return (
       <div>
-        {!this.props.passItems.length > 0 ? null :
-        this.props.passItems.map(item => 
-        <MenItem />
-        )}
+      {this.props.passItems.map(item => 
+        <MenItem passItem={item} key={item.id}/>
+      )}
       </div>
     );
   }
--- client/src/components/Women.js
+++ client/src/components/Women.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import WomenItem from './WomenItem';
 
 class Women extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
-        This is women
+      {this.props.passItems.map(item => 
+        <WomenItem passItem={item} key={item.id}/>
+      )}
       </div>
     );
   }
