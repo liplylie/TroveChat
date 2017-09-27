05bc7a872a29afd15cd5da1dfcbcabda55e52c21
Mark Chen <markchen555@gmail.com>
2017-09-27 16:47:58 -0700

    fix minor issue on NavBar and start building the layout on Dashboard component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,18 +1,42 @@
 // This component will display logged in user's account info
 // Will also route to other user-specific pages (wardrobe, history)
 import React, { Component } from 'react';
-import { NavLink, Route, Link } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Link } from 'react-router-dom';
 import Wardrobe from './Wardrobe';
 
 class Dashboard extends Component {
   render() {
     return (
-        <div>
-        <NavLink exact activeClassName="active" className="nav-link" to='/wardrobe' >
-        My Wardrobe
-        </NavLink>
-        <Route exact path='/wardrobe' component={() => (<Wardrobe />)} />
+      <BrowserRouter>
+        <div className='row dashboard'>
+          <div className='col-mid-4'>
+            <div className='dashboard-wrap'>
+              <div>
+                <span>MY ACCOUNT</span>
+              </div>
+              <div>
+                <NavLink exact activeClassName="active" className="dashboard-link" to='/account' >
+                  Account Dashboard
+                </NavLink>
+                <NavLink exact activeClassName="active" className="dashboard-link" to='/wardrobe' >
+                  My Wardrobe
+                </NavLink>
+              </div>
+            </div>
+          </div>
+          <div className='col-mid-8'>
+            <div className='dashboard-wrap'>
+              <div>
+                <span>MY DASHBOARD</span>
+              </div>
+              <div>
+                <Route exact path='/account' render={() => {return <span>SELECT A TAB ON THE LEFT SIDE TO BEGIN WITH!</span>}} />
+                <Route exact path='/wardrobe' component={() => (<Wardrobe />)} />
+              </div>
+            </div>
+          </div>
         </div>
+      </BrowserRouter>
     );
   }
 }
