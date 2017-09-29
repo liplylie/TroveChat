fbfb51005412389fdc0011534979bc08143a9a7e
kiwook <kiwookiwook@gmail.com>
2017-09-29 16:00:45 -0700

    hide the stripe secret key

--- server/constants/stripe.js
+++ server/constants/stripe.js
@@ -1,8 +1,10 @@
 const configureStripe = require('stripe');
+require('dotenv').config();
+require('dotenv').load();
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
     ? 'sk_live_MY_SECRET_KEY'
-    : 'sk_test_yBe5icC9MTIWVViI73uRX1iO';
+    : process.env.STRIPE_SECRET_KEY;
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
