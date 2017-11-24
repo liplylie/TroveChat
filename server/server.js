5992ece7b41c38e982af31a87d333cebdda160e5
Jordan Daniels <decentpianist@gmail.com>
2017-11-24 14:08:53 -0800

    prepare for heroku deploy

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const socket = require('socket.io');
 const axios = require('axios')
 
 
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 
 app.use(parser.json())
 .use(parser.urlencoded({extended: true}))
